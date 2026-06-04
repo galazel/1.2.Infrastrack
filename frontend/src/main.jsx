7883ea239379a593318a9619de0b16882d29ff89
galazel <glyzelgalagar123@gmail.com>
2026-06-05 00:42:20 +0800

    build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import App from "./App.tsx"
+import App from "./App.jsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { BrowserRouter } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-createRoot(document.getElementById("root")!).render(
+createRoot.render(
   <StrictMode>
     <ThemeProvider>
        <BrowserRouter>
